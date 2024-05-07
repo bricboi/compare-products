507f0bc00ac2979f2ed3db16ad707619423e4f49
bricboi <nikolas@esselius.info>
2024-05-07 09:15:43 +0000

    Add REACT to the workspace

---
+++ compare-products/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Compare producs</h1>
+        <p>
+          This is a website were you can upload pictures and compare different producs
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
